c4971a3dd3e76b120639b525b2ddc12507b4a0bb
Igor Ribeiro Lobo <igorribeirolobo@gmail.com>
2017-09-27 14:36:47 -0300

    ex07

---
+++ src/exercicio07/Main.java
@@ -0,0 +1,28 @@
+package exercicio07;
+
+public class Main {
+
+	public static void main(String[] args) {
+		funcoes f = new funcoes();
+		int n = 0;
+		f.setN(f.lerNumero("Informe um numero inteiro positivo: "));
+		if(f.getN() > 0)
+		{ n = 1;
+		for(int i = 0; i <= f.getN(); i++)
+		{
+			n =  i * n;
+		}
+		}
+		else if(f.getN() < 0)
+		{
+			f.setN(0);
+		}
+		else
+		{
+			f.setN(1);
+		}
+		f.getResultado(n);
+
+	}
+
+}
---
+++ src/exercicio07/funcoes.java
@@ -0,0 +1,38 @@
+package exercicio07;
+
+import javax.swing.JOptionPane;
+
+public class funcoes {
+
+	private int N;
+	
+	public void setN(int N)
+	{
+		this.N = N;
+	}
+	public int getN()
+	{
+		return this.N;
+	}
+	public static int lerNumero(String mensagem)
+	{  int numero = 0;
+	   boolean valida = true;
+	   while(valida)
+	   {
+		   try {
+			   numero = Integer.parseInt(JOptionPane.showInputDialog(mensagem));
+			   valida = false;
+		   }
+		   catch(NumberFormatException e)
+		   {
+			   JOptionPane.showMessageDialog(null, "informe um numero válido, erro: "+ e.toString());
+	       }
+		  
+	   }
+	   return numero;	
+	}
+	public void getResultado(int n)
+	{
+		JOptionPane.showMessageDialog(null,n);
+	}
+}
